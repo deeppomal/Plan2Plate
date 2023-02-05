b4e191c39284a3716a60033571793ae9296d8504
deeppomal <deeppomal98@gmail.com>
2023-02-04 19:23:28 -0500

    DP: Added login and register API

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,75 @@
-console.log('hi')
+require('dotenv').config()
+const express = require('express')
+const mongoose  = require('mongoose')
+const bcrypt = require('bcrypt')
+var User = require('./models/user')
+
+const app = express()
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection
+db.on("error",(error)=>{
+    console.log(error)
+})
+db.once("open",()=>{
+    console.log('connected to DB')
+})
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.get('/',(req,res) => {
+    res.send('Hi')
+})
+
+app.get('/login',(req,res) => {
+    res.send('login')
+})
+
+app.post('/login', async (req,res)=>{
+    try{
+        User.findOne({ username: req.body.username }, function(err, user) {
+            
+            if(user){
+                user.comparePassword(req.body.password, function(err, isMatch) {
+                    if (err) res.status(401).json({message:"Error",error:err}); 
+    
+                    if(isMatch) res.status(201).json({id:user.id,username:user.username,email:user.email})
+                    else res.status(401).json({message:"Invalid password"})
+                });
+            }
+            else{
+                res.status(401).json({message:`No user found with this username: ${req.body.username}`})
+            } 
+           
+        });
+    }
+    catch(err){
+        res.status(400).json({ message: err.message })
+    }
+})
+
+app.get('/register',(req,res) => {
+    res.send('register')
+})
+
+app.post('/register',async(req,res) => {
+    try{
+        var user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        user.save(function(err) {
+            if (err) throw err;
+            res.status(201).json({id:user.id,username:user.username,email:user.email})
+        });
+       
+    }
+    catch(err){
+        res.status(400).json({ message: err.message })
+    }
+})
+
+app.listen(3000)
