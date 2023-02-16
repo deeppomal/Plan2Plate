6634e8daf1360c7be54ae104f3f1d9576601f42b
Mihir Sata <satamihir@gmail.com>
2023-02-15 23:12:49 -0500

    Moved Featured component to components folder

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Featured from '../pages/Featured'
+import Featured from '../components/Featured'
 import FavourableDietList from '../pages/FavourableDietList'
 
 export default function Home() {
