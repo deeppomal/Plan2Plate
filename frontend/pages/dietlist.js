eea316e969f37e57e091928f273839c234afd60e
deeppomal <deeppomal98@gmail.com>
2023-04-18 12:43:52 -0400

    bug fixes

--- frontend/pages/dietlist.js
+++ frontend/pages/dietlist.js
@@ -33,8 +33,8 @@ const FavourableDietList = () => {
           <h2 className="text-2xl font-bold text-gray-900">Favourable Diets</h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-            {callouts.map((callout) => (
-              <Link href="/recipes">
+            {callouts.map((callout,index) => (
+              <Link href="/recipes" key={index}>
                 <div key={callout.name} className="group relative">
                   <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                     <img
